3e479cc1e1176e110b2b00266428c539005910a6
jules <julianegsj@gmail.comgit config --global user.email julianegsj@gmail.com>
2021-05-06 19:34:06 -0300

    adicionar módulo

---
+++ javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,24 @@
+var nome = "Jules"
+var idade = 21
+var possuiFaculdade = false
+
+
+console.log (nome, idade, possuiFaculdade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco)
+
+var sobrenome = 'Rue',
+    cidade = 'Rio'
+
+console.log(sobrenome, cidade)
+
+// var marca = "Dior"
+// marca = "Gucci"
+// console.log(marca)
+
+
+    
---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,28 @@
+// Declarar uma variável com o seu nome
+
+var nome = 'Juliane'
+
+
+// Declarar uma variável com a sua idade
+
+var idade = 21
+
+// Declarar uma variável com a sua comida 
+// favorita e não atribuir valor 
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+var comidaFavorita = 'Lasanha'
+
+// Declarar 5 variáveis diferentes sem valores 
+
+var nome,
+    idade,
+    marca,
+    fonte,
+    livro;
+
+
+console.log (nome,idade, comidaFavorita)
