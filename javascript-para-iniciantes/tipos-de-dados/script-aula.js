9d3d1791fa8341dd109933a84d80c2cedde0bb3c
jules <julianegsj@gmail.comgit config --global user.email julianegsj@gmail.com>
2021-05-06 19:38:24 -0300

    adicionar módulo

---
+++ javascript-para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,38 @@
+var idade = 21
+var simbolo = Symbol()
+console.log(typeof simbolo)
+
+
+
+var nome = 'Jules'
+var sobrenome = 'G.'
+var nomeCompleto = nome + ' ' + sobrenome
+console.log(nomeCompleto)
+
+
+
+// var pontos = 13
+// var frase = 'Anna fez ' + pontos + ' pontos'
+// console.log(typeof frase)
+
+
+
+var ano = '2019'
+var mes = 1
+console.log(ano + mes) // concatena e não soma
+
+
+
+var melhor = 'teste'
+var frase1 = "Esse é o \"melhor\" jogo"
+console.log(frase1)
+
+
+
+var pontos = 13
+var frase2 = `Anna fez ${pontos + 7} pontos`
+
+console.log(frase2)
+
+
+
---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,38 @@
+// Declare uma variável contendo uma string 
+var marca = 'Dior'
+console.log (marca)
+
+
+
+// Declare uma váriavel contendo um número dentro de uma string
+var ano = '2019'
+console.log(ano)
+
+
+
+// Declare uma variável com a sua idade 
+var idade = 21
+console.log(idade)
+
+
+
+// Declare duas  variáveis, uma com seu nome 
+// e outra com seu sobrenome e some as mesmas 
+var nome = 'Juliane'
+var sobrenome = 'Gaudencio'
+//console.log (nome + ' ' + sobrenome)
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto)
+
+
+
+// Coloque a seguinte frase em uma variável: It's time 
+var frase = 'It\'s time' // ou ` `, " "
+console.log (frase)
+
+
+
+// Verifique o tipo da variável que contem seu nome
+var nome = 'Jules'
+console.log(typeof nome)
+
