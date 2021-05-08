e0a8bfb6bf3474776a0221bde4f84eddf4b7be63
jules <julianegsj@gmail.comgit config --global user.email julianegsj@gmail.com>
2021-05-08 00:51:27 -0300

    adicionar módulo

---
+++ javascript-para-iniciantes/numeros-e-operadores/script-aula.js
@@ -0,0 +1,41 @@
+var inteiro = 25
+var pi = 2.5
+var exp = 2e5
+var menosExp = 2e-2
+console.log (inteiro, pi, exp, menosExp)
+
+
+
+
+var divisao = 200 / 5
+var modulo = 31410 % 7
+console.log(divisao, modulo)
+
+
+
+
+var testeNan = 'Isso é 100' / 2
+console.log(testeNan)
+console.log(isNaN(testeNan))
+
+
+
+
+var teste = 10 + 10 + 20 + 20 * 5 + 10
+console.log(teste)
+
+
+
+
+var x = 5 //i ncremento
+console.log(x++) // ou --
+console.log(x)
+
+
+
+
+var idade = '21'
+var somarIdade = 4
+console.log(-idade + somarIdade) 
+// o + e - tenta transformar o valor seguinte em número, 
+// mesmo se for uma string
---
+++ javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,32 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 /2 + 20 // 35
+console.log (total)
+
+
+
+// Crie duas expressões que retornem Nan
+var testeNan = 'Tem 25' * 2 // soma concatena
+console.log(testeNan)
+
+
+
+// Somar a string '200' com o número 50 e retornar 250
+var retorno = +'200'
+var num = 50
+console.log (retorno + num)
+
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5
+//console.log(x++)
+//console.log(--x)
+console.log(++x)
+
+
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2
+var unidade = 'kg'
+var peso = numero + unidade // 80kg
+console.log(peso)
